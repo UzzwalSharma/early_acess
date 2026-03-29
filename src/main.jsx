467750d24c1dc0fcc89949a20d3644b7b883b42c
ujjwal <uzzwal7505@gmail.com>
2026-03-29 19:27:10 +0530

    final

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ConvexReactClient, ConvexProvider } from "convex/react";
-const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
-createRoot(document.getElementById('root')).render(
- 
-    <ConvexProvider client={convex}>
-      <App />
-    </ConvexProvider>
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-)
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
+createRoot(document.getElementById("root")).render(
+  <ConvexProvider client={convex}>
+    <App />
+  </ConvexProvider>,
+);
